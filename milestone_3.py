224d7f62f2efad334f1071c8e8ec0fb4932ceb80
ichaize <isabellechaize1@gmail.com>
2023-03-21 16:46:18 +0000

    Adds an if statement to check if the guess is in the word and combines code from both files so variables are in the right scope.

--- milestone_3.py
+++ milestone_3.py
@@ -1,6 +1,19 @@
+import random
+
+word_list = ["kiwi", "passion fruit", "peach", "pineapple", "mango"]
+print(word_list)
+
+word = random.choice(word_list)
+print(word)
+
 while True:
     guess = input("Please enter a letter")
-    if guess.isalpha() == True:
+    if len(guess) == 1 and guess.isalpha() == True:
         break
     else:
         print("Invalid letter. Please, enter a single alphabetical character.")
+
+if guess in word:
+    print(f"Good guess! {guess} is in the word.")
+else:
+    print(f"Sorry, {guess} is not in the word. Try again.")

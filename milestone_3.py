bf1fd421bee313615d0c50f08cb64b34df4eef52
ichaize <isabellechaize1@gmail.com>
2023-03-21 16:58:24 +0000

    Tidies the code up by creating functions and makes sure guess is always lower case.

--- milestone_3.py
+++ milestone_3.py
@@ -6,14 +6,19 @@ print(word_list)
 word = random.choice(word_list)
 print(word)
 
-while True:
-    guess = input("Please enter a letter")
-    if len(guess) == 1 and guess.isalpha() == True:
-        break
+def check_guess(guess):
+    if guess.lower() in word:
+        print(f"Good guess! {guess} is in the word.")
     else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
+        print(f"Sorry, {guess} is not in the word. Try again.")
 
-if guess in word:
-    print(f"Good guess! {guess} is in the word.")
-else:
-    print(f"Sorry, {guess} is not in the word. Try again.")
+def ask_for_input():
+    while True:
+        guess = input("Please enter a letter")
+        if len(guess) == 1 and guess.isalpha() == True:
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+    check_guess(guess)
+
+ask_for_input()

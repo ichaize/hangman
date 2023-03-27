82c82b774a2a6274e5a466acf0b25bf7cbab0a3a
ichaize <isabellechaize1@gmail.com>
2023-03-27 17:13:21 +0100

    adds check_guess and _ask_for_input methods to the Hangman class. The check_guess method is incomplete.

--- milestone_4.py
+++ milestone_4.py
@@ -8,3 +8,29 @@ class Hangman():
         self.word_guessed = ["_"] * len(self.word)
         self.num_letters = len(set(self.word))
         self.list_of_guesses = []
+
+    def check_guess(self, guess):
+        if guess.lower() in self.word:
+            print(f"Good guess! {guess} is in the word.")
+    
+    def ask_for_input(self):
+        while True:
+            guess = input("Please enter a letter")
+            if len(guess) != 1 or guess.isalpha() != True:
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+                break
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
+
+
+fruit_list = ["kiwi", "passion fruit", "peach", "pineapple", "mango"]
+fruit_game = Hangman(fruit_list)
+fruit_game.ask_for_input()
+
+
+
+
+

f1677eed2da4e8b1932994c5ee1f805e17d4381f
ichaize <isabellechaize1@gmail.com>
2023-03-27 16:47:20 +0100

    Adds self to all attributes

--- milestone_4.py
+++ milestone_4.py
@@ -4,7 +4,7 @@ class Hangman():
     def __init__(self, word_list, num_lives=5):
         self.word_list = word_list
         self.num_lives = num_lives
-        word = random.choice(self.word_list)
-        word_guessed = ["_"] * len(word)
-        num_letters = None
-        list_of_guesses = []
+        self.word = random.choice(self.word_list)
+        self.word_guessed = ["_"] * len(word)
+        self.num_letters = None
+        self.list_of_guesses = []

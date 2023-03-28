2d01c653b1caf58cadbfb19150ffcf376a2a7a59
ichaize <isabellechaize1@gmail.com>
2023-03-28 12:56:00 +0100

    Adds break to the num_lives checker

--- milestone_5.py
+++ milestone_5.py
@@ -39,8 +39,9 @@ def play_game(word_list):
     num_lives = 5
     game = Hangman(word_list, num_lives)
     while True:
-        if num_lives == 0:
+        if game.num_lives == 0:
             print("You lost!")
+            break
         elif game.num_letters > 0:
             game.ask_for_input()
         elif num_lives != 0 and game.num_letters == 0:
